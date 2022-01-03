d4db8cc6037019dd946ae147cf6d32c76f9d46ff
kleborgn <kikilebor@live.fr>
2022-01-03 21:15:34 +0100

    added forme selection and capitalize

--- src/convert.py
+++ src/convert.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-test = "Tous les acheteur"
+test = "Tous les artistes sont là"
 
 data_file = os.path.join("data", "dico.json")
 with open(__file__.replace("convert.py", data_file)) as f:
@@ -17,15 +17,23 @@ def getMascWords():
         out.append(word['MASC'])
     return out
 
-def convert(splittedText):
+def convert(splittedText, forme):
     out = splittedText
     d = getMascWords()
     for word in out:
-        if word.lower() in d:
-            out[out.index(word)] = data[d.index(word.lower())]['forme1']
+        if word in d:
+            out[out.index(word)] = data[d.index(word)][forme]
+        elif word.lower() in d:
+            out[out.index(word)] = data[d.index(word.lower())][forme].capitalize()
+        
+        elif (word[:-1] in d) and (word[-1]=='s'):
+            out[out.index(word)] = data[d.index(word[:-1])][forme].capitalize() + 's'
+        elif (word[:-1].lower() in d) and (word[-1]=='s'):
+            out[out.index(word)] = data[d.index(word[:-1].lower())][forme].capitalize() + 's'
 
     return out
 
-def convert_sentence(sentence):
-    return convert(extractWords(sentence))
-print(convert(extractWords('tous')))
+def convert_sentence(sentence, forme):
+    return ' '.join(convert(extractWords(sentence), forme))
+
+print(convert_sentence(test, 'forme1'))
